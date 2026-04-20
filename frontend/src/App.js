6ee830485717a54f25edb07561be591d3547c593
KuldeepIsharwal <kisharwal7@gmail.com>
2026-04-21 00:13:26 +0530

    Fixed small bug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/journal';
+const API_URL = 'https://arvyax-api.onrender.com/api/journal';
 const USER_ID = '123'; // Hardcoded for this demo
 
 function App() {
